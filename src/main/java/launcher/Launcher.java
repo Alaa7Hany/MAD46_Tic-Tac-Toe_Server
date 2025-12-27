a86251bde6ed68f395026543d537fa0ec76c8d1d
Alaa7Hany <3laahanylol@gmail.com>
2025-12-27 18:27:19 +0200

    Prepared to produce fat jar

---
+++ src/main/java/launcher/Launcher.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package launcher;
+
+import com.mycompany.mad46_tic.tac.toe_server.App;
+
+/**
+ *
+ * @author hp
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
